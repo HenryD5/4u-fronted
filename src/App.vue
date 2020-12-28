935e9e93f94a9dfef2bba8bfd1bd785bf7140717
Henry Yamunaque <david_yamunaque_5@outlook.com>
2020-12-27 23:21:40 -0500

    uu

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <div class="text-center">uu</div>
     <router-view/>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,8 @@ const routes = [
 ]
 
 const router = new VueRouter({
+  mode: 'history',
+  authRedirect: { path: '/' },
   routes
 })
 
